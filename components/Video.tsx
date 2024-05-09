587b00d664e8ea66de1d8131411b3fda9b79155b
luiz felipe <luizfelipebragaa@gmail.com>
2024-05-09 14:57:34 -0300

    feat: video widget

---
+++ components/Video.tsx
@@ -0,0 +1,29 @@
+import { VideoWidget } from "apps/admin/widgets.ts";
+import Video from "apps/website/components/Video.tsx";
+
+export interface Props {
+  video: VideoWidget;
+  src: string;
+  title: string;
+}
+
+function VideoComponent(props: Props) {
+  console.log("teste");
+  return (
+    <div class="flex justify-center items-center h-screen ">
+      <div class="w-3/4 max-w-screen-lg">
+        <h2>{props.title}</h2>
+        <Video
+          class="w-full"
+          src={props.src}
+          controls={true}
+          height={800}
+          width={800}
+          autoPlay
+        />
+      </div>
+    </div>
+  );
+}
+
+export default VideoComponent;
--- fresh.gen.ts
+++ fresh.gen.ts
@@ -24,6 +24,7 @@ import * as $ProductImageZoom from "./islands/ProductImageZoom.tsx";
 import * as $SearchControls from "./islands/SearchControls.tsx";
 import * as $ShippingSimulation from "./islands/ShippingSimulation.tsx";
 import * as $ShowMore from "./islands/ShowMore.tsx";
+import * as $Video from "./islands/Video.tsx";
 import * as $WishlistButton_vtex from "./islands/WishlistButton/vtex.tsx";
 import * as $WishlistButton_wake from "./islands/WishlistButton/wake.tsx";
 import { type Manifest } from "$fresh/server.ts";
@@ -54,6 +55,7 @@ const manifest = {
     "./islands/SearchControls.tsx": $SearchControls,
     "./islands/ShippingSimulation.tsx": $ShippingSimulation,
     "./islands/ShowMore.tsx": $ShowMore,
+    "./islands/Video.tsx": $Video,
     "./islands/WishlistButton/vtex.tsx": $WishlistButton_vtex,
     "./islands/WishlistButton/wake.tsx": $WishlistButton_wake,
   },
